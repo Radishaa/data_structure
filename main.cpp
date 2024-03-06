90a450104208f3595b4a0b5ec78e57556c234605
“Radishaa” <galaxy.comet@qq.com>
2024-03-07 02:31:18 +0800

    add: test code

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+// #include <string>
+// #include <cmath>
+#include "include/RSort.h"
+using namespace std;
+
+int main()
+{
+    //快排测试
+    // RSort t;
+    // vector<int> a = {2, 4, 1, 0, 3, 5, 5, 21, 52, 79};
+    // t.qsort(a, 0, a.size() - 1);
+    // for (size_t i = 0; i < a.size(); i++)
+    // {
+    //     cout << a[i] << endl;
+    // }
+
+    return 0;
+}
