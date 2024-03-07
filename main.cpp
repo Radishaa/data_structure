cc963d5f400ec69a775e2140577cf4ca9e830cb8
“Radishaa” <galaxy.comet@qq.com>
2024-03-07 14:28:26 +0800

    add: merge sort test

--- main.cpp
+++ main.cpp
@@ -6,14 +6,17 @@ using namespace std;
 
 int main()
 {
-    //快排测试
-    // RSort t;
-    // vector<int> a = {2, 4, 1, 0, 3, 5, 5, 21, 52, 79};
-    // t.qsort(a, 0, a.size() - 1);
-    // for (size_t i = 0; i < a.size(); i++)
-    // {
-    //     cout << a[i] << endl;
-    // }
+    // 排序算法测试
+    RSort t;
+    vector<int> a = {2, 4, 1, 0, 3, 5, 5, 21, 52, 79};
+    // 快排测试
+    //  t.qsort(a, 0, a.size() - 1);
+    t.msort(a, 0, a.size() - 1);
+    // 排序结果展示
+    for (size_t i = 0; i < a.size(); i++)
+    {
+        cout << a[i] << endl;
+    }
 
     return 0;
 }
