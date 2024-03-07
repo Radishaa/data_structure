a3b51450d421c362f4522d44a505b6ca91df705f
“Radishaa” <galaxy.comet@qq.com>
2024-03-07 14:28:02 +0800

    add: merge sort head

--- include/RSort.h
+++ include/RSort.h
@@ -9,6 +9,7 @@ private:
      * @brief 用于排序交换vector<int>中的两个数
      */
     void swap(std::vector<int> &nums, int i, int j);
+
     /**
      * @param left 划分开始位置
      * @param right 划分结束位置
@@ -16,14 +17,32 @@ private:
      */
     int partition(std::vector<int> &nums, int left, int right);
 
+    /**
+     * @param left 待合并开始位置
+     * @param mid 待合并中间数
+     * @param right 待合并结束位置
+     * @brief 实现归并排序的合并 
+    */
+    void merge(std::vector<int> &nums, int left, int mid, int right);
+
 public:
     RSort(); // 构造函数
     /**
+     * @param nums 待排序数组
      * @param left 排序开始位置
      * @param right 排序结束位置
-     * @brief 实现快速排序
+     * @brief 快速排序
      */
     void qsort(std::vector<int> &nums, int left, int right);
+
+    /**
+     * @param nums 待排序数组
+     * @param left 排序开始位置
+     * @param right 排序结束位置 
+     * @brief 归并排序
+    */
+    void msort(std::vector<int> &nums, int left, int right);
+
 };
 
 #endif
