974acb824288110b05c18f64a76d39b2d1fbb98a
“Radishaa” <galaxy.comet@qq.com>
2024-03-08 12:32:33 +0800

    test: heap sort

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main()
     vector<int> a = {2, 4, 1, 0, 3, 5, 5, 21, 52, 79};
     // 快排测试
     //  t.qsort(a, 0, a.size() - 1);
-    t.msort(a, 0, a.size() - 1);
+    t.heapsort(a);
     // 排序结果展示
     for (size_t i = 0; i < a.size(); i++)
     {
