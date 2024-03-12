2986294684bfdefe23067f4964abc0bb0f937c39
“Radishaa” <1536857028@qq.com>
2024-03-12 23:11:52 +0800

    test: avl tree

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 // #include <string>
 // #include <cmath>
 #include "include/RSort.h"
+#include "include/RAVLTree.h"
 using namespace std;
 
 int main()
@@ -13,10 +14,21 @@ int main()
     //  t.qsort(a, 0, a.size() - 1);
     t.shellsort(a);
     // 排序结果展示
-    for (size_t i = 0; i < a.size(); i++)
-    {
-        cout << a[i] << endl;
-    }
+    // for (size_t i = 0; i < a.size(); i++)
+    // {
+    //     cout << a[i] << endl;
+    // }
+    RAVLTree *root = new RAVLTree(2);
+    root = root->insert(root, 10);
+    root = root->insert(root, 5);
+    root = root->insert(root, 3);
+    root = root->insert(root, 9);
+    root = root->insert(root, 22);
+    root = root->insert(root, 33);
+    root = root->insert(root, 44);
+    root = root->remove(root, 2);
+    root->inOrder(root);
+    std::cout << root->serarch(root, 5)->val << std::endl;
 
     return 0;
 }
